d2c21730f7580fa1f1b29b00c255d0ccd297dff2
Ian Keough <ian.keough@autodesk.com>
2015-07-13 22:12:58 -0700

    Move sample projects into their new home.

---
+++ src/SampleLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleLibraryTests")]
+[assembly: Guid("c3d8c6fc-62ad-4272-91a9-492be76577e5")]
