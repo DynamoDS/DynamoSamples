87c954bad7ee08c8536740458623da97bc982aa3
Michael Kirschner <mjk.kirschner@gmail.com>
2018-02-06 13:42:25 -0500

    add new project extension which is used for testing

---
+++ src/SampleExtension/Extension.cs
@@ -0,0 +1,58 @@
+﻿using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+using Dynamo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleExtension
+{
+    public class Extension : IExtension
+    {
+        public List<NodeModel> nodes = new List<NodeModel>();
+        public bool readyCalled = false;
+
+        public string Name
+        {
+            get
+            {
+                return "testExtension";
+            }
+        }
+
+        public string UniqueId
+        {
+            get
+            {
+                return Guid.NewGuid().ToString();
+            }
+        }
+
+        public void Dispose()
+        {
+           
+        }
+
+        public void Ready(ReadyParams sp)
+        {
+            sp.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodeAdded;
+            this.readyCalled = true;
+        }
+
+        private void CurrentWorkspaceModel_NodeAdded(Dynamo.Graph.Nodes.NodeModel obj)
+        {
+            this.nodes.Add(obj);
+        }
+
+        public void Shutdown()
+        {
+           
+        }
+
+        public void Startup(StartupParams sp)
+        {
+        
+        }
+    }
+}
