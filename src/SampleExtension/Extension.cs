640827746a104761b4ade88d297225767a51f12f
Michael Kirschner <mjk.kirschner@gmail.com>
2018-02-26 13:33:22 -0500

    coments update csproj

--- src/SampleExtension/Extension.cs
+++ src/SampleExtension/Extension.cs
@@ -8,6 +8,16 @@ using System.Text;
 
 namespace SampleExtension
 {
+    /// <summary>
+    /// The Extension framework for Dynamo allowed you to extend
+    /// Dynamo by loading your own classes that can interact with Dynamo's API.
+    /// An Extension has two components, an assembly containing your class and
+    /// an xml manifest file telling Dynamo where to find your assembly. Extension
+    /// manifests are loaded from the Dynamo/Extensions folder or from package/extra
+    /// folders.
+    /// 
+    /// This sample demonstrates a simple IExtension which tracks nodes added to the workspace.
+    /// </summary>
     public class Extension : IExtension
     {
         public List<NodeModel> nodes = new List<NodeModel>();
@@ -33,7 +43,12 @@ namespace SampleExtension
         {
            
         }
-
+        /// <summary>
+        /// Ready is called when the DynamoModel is finished being built, or when the extension is installed
+        /// sometime after the DynamoModel is alreay built. ReadyParams provide access to references like the
+        /// CurrentWorkspace.
+        /// </summary>
+        /// <param name="sp"></param>
         public void Ready(ReadyParams sp)
         {
             sp.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodeAdded;
