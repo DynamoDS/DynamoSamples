058777fa98d91c7ecd39db09895858f8cb8081f1
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-12-11 16:55:56 -0500

    Update SampleViewExtension with new features (#36)
    The SampleViewExtension is updated to use the extension sidebar API and
    also implement the Closed method to update the status of its menu item.

--- README.md
+++ README.md
@@ -9,16 +9,21 @@ These samples make use of the [Dynamo NuGet packages](https://www.nuget.org/pack
 
 # Building the Samples
 
+## Requirements
+- Visual Studio 2019
+- .NET Framework 4.8
+
+## Instructions
 - Clone the repository.
 - Choose a branch:
   - The master branch of Dynamo Samples corresponds to the master branch of Dynamo. To build against a specific version, choose that version's branch. I.e. 0.8.0, 0.9.0, etc.
-- In VisualStudio 2013 or greater, open DynamoSamples.2013.sln.
-- Build the Debug/Any CPU configuration.
-- The `dynamo_package` folder at the root of the repository will now have the built libraries. The `Dynamo Samples` folder in that directory can be copied directly to your Dynamo packages directory:`C:\Users\<you>\AppData\Roaming\Dynamo\0.8\packages`.
+- Open `DynamoSamples.sln` with Visual Studio.
+- Build using the `Debug/Any CPU` configuration.
+- The `dynamo_package` folder at the root of the repository will now have the built libraries. The `Dynamo Samples` folder in that directory can be copied directly to your Dynamo packages directory:`C:\Users\<you>\AppData\Roaming\Dynamo Core\<version>\packages`.
 - To install the sample view extension the `SampleViewExtension\bin\debug` folder (or release) should contain
   - `SampleViewExtension.dll` which should be copied to your root Dynamo build location
   - `SampleViewExtension_ViewExtensionDefinition` which should be copied to the `viewExtensions` folder inside your root Dynamo build location
-- Run Dynamo. You should find `SampleLibraryUI` and `SampleLibraryZeroTouch` categories in your library and the `view` tab inside of Dynamo should now contain `Show View Extension Sample Window`.
+- Run Dynamo. You should find `SampleLibraryUI` and `SampleLibraryZeroTouch` categories in your library and the `View` tab inside of Dynamo should now contain `Show View Extension Sample Window`.
 
 Assembly Reference
 Path to assembly for binaries are defined in CS.props and user_local.props which can be found at $(SolutionDirectory)\Config
--- src/SampleViewExtension/SampleViewExtension.cs
+++ src/SampleViewExtension/SampleViewExtension.cs
@@ -9,69 +9,72 @@ namespace SampleViewExtension
     /// The View Extension framework for Dynamo allows you to extend
     /// the Dynamo UI by registering custom MenuItems. A ViewExtension has 
     /// two components, an assembly containing a class that implements 
-    /// IViewExtension, and an ViewExtensionDefintion xml file used to 
-    /// instruct Dynamo where to find the class containing the
-    /// IViewExtension implementation. The ViewExtensionDefinition xml file must
+    /// IViewExtension or extends ViewExtensionBase, and a ViewExtensionDefinition
+    /// XML file used to instruct Dynamo where to find the class containing the
+    /// View Extension implementation. The ViewExtensionDefinition XML file must
     /// be located in your [dynamo]\viewExtensions folder.
     /// 
-    /// This sample demonstrates an IViewExtension implementation which 
-    /// shows a modeless window when its MenuItem is clicked. 
+    /// This sample demonstrates a View Extension implementation which inherits from
+    /// ViewExtensionBase. It adds a user interface to Dynamo's extension sidebar
+    /// when its MenuItem is clicked. 
     /// The Window created tracks the number of nodes in the current workspace, 
     /// by handling the workspace's NodeAdded and NodeRemoved events.
     /// </summary>
-    public class SampleViewExtension : IViewExtension
+    public class SampleViewExtension : ViewExtensionBase
     {
         private MenuItem sampleMenuItem;
 
-        public void Dispose()
+        public override void Dispose()
         {
         }
 
-        public void Startup(ViewStartupParams p)
+        public override void Startup(ViewStartupParams p)
         {
         }
 
-        public void Loaded(ViewLoadedParams p)
+        public override void Loaded(ViewLoadedParams p)
         {
             // Save a reference to your loaded parameters.
             // You'll need these later when you want to use
             // the supplied workspaces
 
-            sampleMenuItem = new MenuItem {Header = "Show View Extension Sample Window"};
-            sampleMenuItem.Click += (sender, args) =>
+            var viewModel = new SampleWindowViewModel(p);
+            var window = new SampleWindow
             {
-                var viewModel = new SampleWindowViewModel(p);
-                var window = new SampleWindow
-                {
-                    // Set the data context for the main grid in the window.
-                    MainGrid = { DataContext = viewModel },
+                // Set the data context for the main grid in the window.
+                MainGrid = { DataContext = viewModel },
 
-                    // Set the owner of the window to the Dynamo window.
-                    Owner = p.DynamoWindow
-                };
-
-                window.Left = window.Owner.Left + 400;
-                window.Top = window.Owner.Top + 200;
-
-                // Show a modeless window.
-                window.Show();
+                // Set the owner of the window to the Dynamo window.
+                Owner = p.DynamoWindow
             };
+
+            sampleMenuItem = new MenuItem { Header = "Show View Extension Sample Window", IsCheckable = true };
+            sampleMenuItem.Checked += (sender, args) => p.AddToExtensionsSideBar(this, window);
+            sampleMenuItem.Unchecked += (sender, args) => p.CloseExtensioninInSideBar(this);
             p.AddMenuItem(MenuBarType.View, sampleMenuItem);
         }
 
-        public void Shutdown()
+        public override void Shutdown()
         {
         }
 
-        public string UniqueId
+        public override void Closed()
+        {
+            if (sampleMenuItem != null)
+            {
+                sampleMenuItem.IsChecked = false;
+            }
+        }
+
+        public override string UniqueId
         {
             get
             {
-                return Guid.NewGuid().ToString();
+                return "61BB15A8-9B6B-4AB0-8D75-F7C34E0B9112";
             }  
         } 
 
-        public string Name
+        public override string Name
         {
             get
             {
