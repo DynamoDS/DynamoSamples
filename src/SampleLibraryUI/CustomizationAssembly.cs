e57247116825c1e23d64a5aa0d0fcf7c5444f8b0
Deyan Nenov <dnenov@archilizer.com>
2025-10-01 17:54:13 +0100

    nugets bump to 2685

---
+++ src/SampleLibraryUI/CustomizationAssembly.cs
@@ -0,0 +1,13 @@
+using System.Reflection
+
+
+[assembly: AssemblyTitle("SampleLibraryUI.customization")]
+[assembly: AssemblyDescription("Customization assembly for SampleLibraryUI")]
+
+namespace SampleLibraryUI
+{
+    public static class CustomizationAssembly
+    {
+        // This assembly contains resources for SampleLibraryUI
+    }
+}
